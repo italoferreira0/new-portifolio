385c16108a9c9b796c35173c4593658e9b3b0c1d
Italo <italofsilva583@gmail.com>
2024-07-16 18:49:29 -0300

    teste apresent

--- src/componentes/Apresentacao/Apresentacao.jsx
+++ src/componentes/Apresentacao/Apresentacao.jsx
@@ -10,11 +10,7 @@ export default function Apresentacao(props) {
     return(
         <section className='container-md Apresentacao' id='home'>
             <div class="row">
-                <div className="col-sm DivFoto">
-                    <img src={minhaFoto} alt="Minha Foto" title='Italo Ferreira' className='minhaFt img-fluid'/>
-                    <button className='btn Downlaod mt-4'><a  download>Download CV</a></button>
-                </div>
-                
+                <h1>Testes</h1>
             </div>
     
                         
