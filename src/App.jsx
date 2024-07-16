bfd194fe0b54fc3662b47b03c18f8bef40fa67df
Italo <italofsilva583@gmail.com>
2024-07-16 18:53:21 -0300

    teste

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-import Apresentacao from './componentes/Apresentacao/Apresentacao';
-
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
-      <Apresentacao/>
+      <h1>Teste</h1>
+      
     </>
   )
 }
