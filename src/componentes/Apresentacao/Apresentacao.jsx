7e425d91e8ec6d95e624db129fb8fda49a50da1b
Italo <italofsilva583@gmail.com>
2024-07-23 13:16:55 -0300

    Atualização Projetos

--- src/componentes/Apresentacao/Apresentacao.jsx
+++ src/componentes/Apresentacao/Apresentacao.jsx
@@ -7,17 +7,18 @@ import curriculo from './curriculo.pdf'
 export default function Apresentacao(props) {
 
     return (
-        <div className='container ' id='home'>
-            <div className="row align-items-center mt-3">
+        <div className='container mb-5' id='home'>
+            <div className="row align-items-center mt-1">
                 <div className="col-sm-12 col-md-6 text-center text-md-left">
                     <img src={minhaFoto} alt="Minha Foto" title='Italo Ferreira' className='minhaFt img-fluid mt-5' />
-                    <a href={curriculo} download><button className='btn Downlaod mt-4'>Download CV</button></a>
+                    <a href={curriculo} download><button className='btn Downlaod mt-2'>Download CV</button></a>
                 </div>
                 <div className="col-sm-12 col-md-6 text-center text-md-left mt-5">
                     <h3>Ola, eu sou Italo.</h3>
                     <h3>Bem-vindo ao meu portifólio.</h3>
                 </div>
             </div>
+            <br />
         </div>
 
 
--- src/componentes/Projetos/Projetos.jsx
+++ src/componentes/Projetos/Projetos.jsx
@@ -1,47 +1,39 @@
-import './Projetos.css'
-// import imc from './img/imc.png'
-
+import './Projetos.css';
+// import imc from './img/imc.png';
 
 export default function Projetos(props) {
     return (
-        <div className='container text-center mt-5' id='projetos' >
+        <div className='container text-center mt-5' id='projetos'>
             <br />
-            <div class="row justify-content-center mt-5">
-                <div class="col-md-4 Projeto mt-5" id='ProjetoIMC'>
-                    <div className="d-flex justify-content-center align-items-end DivIconsProjeto">
-                        <a target="_blank" href="https://github.com/italoferreira0/imc-react" className="icon-git ">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" className="bi bi-github" viewBox="0 0 16 16">
-                                <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
-                            </svg>
-                        </a>
-                        <a target="_blank" href="https://imc-react-eight.vercel.app/" className="icon-deploy">
-                            <img  width="40" height="40" src="https://img.icons8.com/?size=100&id=1345&format=png&color=000000" alt="" />
-                        </a>
-
-                        
-                    </div>
-
+            <h3 className='mt-5'>Projetos</h3>
+            <div className="row justify-content-center mt-4">
+                <div className="col-md-4 Projeto" id='ProjetoIMC'>
+                 
+                   <div className="button-group">
+                       <button type="button" className="btn btn-dark"><a href="">Deploy</a></button>
+                       <button type="button" className="btn btn-dark"><a href="">Github</a></button>
+                   </div>
                 </div>
-                <div class="col-md-4 Projeto mt-5">
-                    <h3>Simulador de OEE</h3>
+
+                <div className="col-md-4 Projeto">
+                    <h3>Em breve...</h3>
                 </div>
-                <div class="col-md-4 Projeto mt-5">
-                    <h3>Em Breve...</h3>
+                <div className="col-md-4 Projeto">
+                    <h3>Em breve...</h3>               
                 </div>
             </div>
+            <div className="row justify-content-center mt-5">
+                <div className="col-md-4 Projeto">
+                   <h3>Em breve...</h3>
+                </div>
 
-            <div class="row justify-content-center">
-                <div class="col-md-4 Projeto">
-                    <h3>Em Breve...</h3>
+                <div className="col-md-4 Projeto">
+                    <h3>Em breve...</h3>
                 </div>
-                <div class="col-md-4 Projeto">
-                    <h3>Em Breve...</h3>
-                </div>
-                <div class="col-md-4 Projeto">
-                    <h3>Em Breve...</h3>
+                <div className="col-md-4 Projeto">
+                    <h3>Em breve...</h3>               
                 </div>
             </div>
         </div>
-
-    )
+    );
 }
